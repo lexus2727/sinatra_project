297c32a4f54cb60b7ccd90bbb142f3727fe68393
lexus2727 <lexus2424@yahoo.com>
2019-08-30 12:48:18 -0400

    editing models

--- app/models/trek.rb
+++ app/models/trek.rb
@@ -2,4 +2,8 @@ class Trek < ActiveRecord::Base
     belongs_to :user
     has_many :lists
     validates_presence_of :name, :location
+
+    def trek_locator
+    User.find_by(id: user_id).username
+    end
 end
