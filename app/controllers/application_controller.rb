9a54c97a4e2e7936de1ec08771fa343d7016a4a9
lexus2727 <lexus2424@yahoo.com>
2019-09-15 08:28:43 -0400

    final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,20 +14,17 @@ class ApplicationController < Sinatra::Base
   end
 
 
-  # get '/treks/index' do
-  #   erb :"/treks/index"
-  # end
-
+ 
   
 
 
   
-helpers do
-  def is_logged_in?
+  helpers do
+   def is_logged_in?
     !!session[:user_id]
   end
 
-  def current_user
+   def current_user
     User.find(session[:user_id])
   end
  end
