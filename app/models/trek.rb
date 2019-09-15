219e060ef822af2ab64f2049f4cf912d3399f9fa
lexus2727 <lexus2424@yahoo.com>
2019-09-14 21:46:23 -0400

    final

--- app/models/trek.rb
+++ app/models/trek.rb
@@ -1,10 +1,4 @@
 class Trek < ActiveRecord::Base
     belongs_to :user
     validates_presence_of :name, :location
-
-    
-    def trek_creator
-    User.find_by(id: user_id).username
-      end
-   
 end
